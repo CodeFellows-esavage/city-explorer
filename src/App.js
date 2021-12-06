ddca2540649a24545cbb410ff6e7316b4f002aee
erik savage <erik.k.savage@gmail.com>
2021-12-06 15:57:50 -0800

    adds Header, Main, Footer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import Header from "./components/Header.js";
+import Main from "./components/Main.js";
+import Footer from "./components/Footer.js";
+
 
 class App extends Component {
   render(){
     return(
-      <h1>Hello World!</h1>
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
     )
   }
 }
