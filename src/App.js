70a516e99538fb868fa392f0bd2bb4114792121d
erik savage <erik.k.savage@gmail.com>
2021-12-06 16:17:02 -0800

    adds axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Header from "./components/Header.js";
 import Main from "./components/Main.js";
 import Footer from "./components/Footer.js";
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      locationQuery =
+    }
+  }
   render(){
     return(
       <>
