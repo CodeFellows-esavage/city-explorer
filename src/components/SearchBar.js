9533c0fb5b190fe6792618215b769d303ceca34e
erik savage <erik.k.savage@gmail.com>
2021-12-06 16:52:23 -0800

    adds search bar form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,27 @@
 import { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 class SearchBar extends Component {
     render() {
         return (
-            <h1> Search Bar</h1>
+            <Form onSubmit={this.locationSubmit}>
+                <Row>
+                    <Col xs={7}>
+                        <Form.Control name="city" placeholder="City" />
+                    </Col>
+                    {/* <Col>
+                        <Form.Control placeholder="State" />
+                    </Col> */}
+                    <Col>
+                        <Button type="submit" className="mb-2">
+                            Explore!
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
         )
     }
 }
