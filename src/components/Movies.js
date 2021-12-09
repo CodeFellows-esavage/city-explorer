bb9078e199f1c908d9ff42de6c0ec599fcf5c5de
erik savage <erik.k.savage@gmail.com>
2021-12-09 11:14:51 -0800

    renames component from Movie to Movies

---
+++ src/components/Movies.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+
+class Movies extends Component{
+    render(){
+        return(
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.props.movieArr.image_url} />
+                <Card.Body>
+                    <Card.Title>{this.props.movieArr.title}</Card.Title>
+                    <Card.Text>
+                        {this.props.movieArr.overview}
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem>{this.props.movieArr.released_on}</ListGroupItem>
+                    <ListGroupItem>{this.props.movieArr.popularity}</ListGroupItem>
+                    <ListGroupItem>{this.props.movieArr.average_votes}</ListGroupItem>
+                    <ListGroupItem>{this.props.movieArr.total_votes}</ListGroupItem>
+                </ListGroup>
+            </Card>
+        )
+    }
+}
+
+export default Movies;
