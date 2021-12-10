819f4ede26f3c3f6e2f2565107e3cf30fa354bb0
erik savage <erik.k.savage@gmail.com>
2021-12-09 17:39:38 -0800

    sets consistent size for moviedetail component, adds scrolling to overview

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -6,11 +6,11 @@ import ListGroupItem from 'react-bootstrap/ListGroupItem';
 class MovieDetail extends Component{
     render(){
         return(
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '18rem'}}>
                 <Card.Img variant="top" src={this.props.movieArr.image_url} />
                 <Card.Body>
                     <Card.Title>{this.props.movieArr.title}</Card.Title>
-                    <Card.Text>
+                    <Card.Text style={{height:'12rem', overflow: 'scroll'}}>
                         {this.props.movieArr.overview}
                     </Card.Text>
                 </Card.Body>
