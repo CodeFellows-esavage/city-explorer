ab657031bc809e183c1d08763474a425c66df118
erik savage <erik.k.savage@gmail.com>
2021-12-09 17:02:48 -0800

    adds WeatherDay component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import Row from 'react-bootstrap/Row';
 import LocationCard from './LocationCard.js';
 import Weather from './Weather.js';
 import Movies from './Movies.js';
@@ -9,18 +8,8 @@ class Main extends Component {
         return (
             <>
                 {this.props.locationObj.display_name ? <LocationCard locationObj={this.props.locationObj}/> : <h3>Enter a location to explore, to find local details...</h3>}
-                <Row style={{width: '54rem', marginLeft: '0rem'}}>
-                    {this.props.forcastArr ? this.props.forcastArr.map(forcastObj => {
-                        return <Weather forcast={forcastObj} key={forcastObj.date}/>    
-                    }) : <small style={{textAlign: 'center', margin: '5px 0'}}>No forcast data for this location...</small>
-                    }
-                </Row>
-                <Row style={{width: '54rem', marginLeft: '0rem'}}>
-                    {this.props.movieArr ? this.props.movieArr.map(movieObj => {
-                        return <Movies movieArr={movieObj} key={movieObj.title}/>}) 
-                        : <small style={{textAlign: 'center', margin: '5px 0'}}>No movie data for this location...</small>
-                    }
-                </Row>
+                <Weather forcastArr={this.props.forcastArr}/>
+                <Movies movieArr={this.props.movieArr}/>
             </>    
         )
     }
--- src/components/WeatherDay.js
+++ src/components/WeatherDay.js
@@ -1,9 +1,18 @@
 import { Component } from "react";
+import Card from 'react-bootstrap/Card';
 
 class WeatherDay extends Component{
     render(){
         return(
-
+            <Card style={{ width: '18rem', textAlign: 'center'}}>
+                <Card.Body>
+                    <small className="text-muted">Weather Forcast For:</small>
+                    <Card.Title>{this.props.forcast.date}</Card.Title>
+                    <Card.Text>
+                        {this.props.forcast.description}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         )
     }
 }
