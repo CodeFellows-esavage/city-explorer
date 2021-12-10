98afa6de6da9938dc0ae2e5119be1b63965b9062
erik savage <erik.k.savage@gmail.com>
2021-12-09 17:01:50 -0800

    adds MovieDetail component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,26 +1,17 @@
 import { Component } from 'react';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import Row from 'react-bootstrap/Row';
+import MovieDetail from './MovieDetail'
+
 
 class Movies extends Component{
     render(){
         return(
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={this.props.movieArr.image_url} />
-                <Card.Body>
-                    <Card.Title>{this.props.movieArr.title}</Card.Title>
-                    <Card.Text>
-                        {this.props.movieArr.overview}
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>{this.props.movieArr.released_on}</ListGroupItem>
-                    <ListGroupItem>{this.props.movieArr.popularity}</ListGroupItem>
-                    <ListGroupItem>{this.props.movieArr.average_votes}</ListGroupItem>
-                    <ListGroupItem>{this.props.movieArr.total_votes}</ListGroupItem>
-                </ListGroup>
-            </Card>
+            <Row style={{width: '54rem', marginLeft: '0rem'}}>
+                {this.props.movieArr ? this.props.movieArr.map(movieObj => {
+                    return <MovieDetail movieArr={movieObj} key={movieObj.title}/>}) 
+                    : <small style={{textAlign: 'center', margin: '5px 0'}}>No movie data for this location...</small>
+                }
+            </Row>
         )
     }
 }
